16f4227eabf8dd0eb75ffba2f709488092ffbe0a
sungwoo.kim <tjddn5242@gmail.com>
2023-01-17 01:31:30 +0900

    2

--- example/example.py
+++ example/example.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from audiorecorder import audiorecorder
+import speech_recognition as sr
 
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Recording...")
@@ -11,6 +12,16 @@ if len(audio) > 0:
     # To save audio to a file:
     wav_file = open("audio.wav", "wb")
     wav_file.write(audio.tobytes())
+
+r = sr.Recognizer()
+
+
+with wav_file as source:
+    audio2 = r.record(source)
+
+
+result = r.recognize_google(audio2, language = 'ko-KR', show_all=True)
+print(result['alternative'][0]['transcript'])
 
 text_output = st.empty()
 text_output.markdown(f"**Text:** ")
