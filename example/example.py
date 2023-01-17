9d652dbf59f6901eb4d6831e44bff748795c2db5
sungwoo.kim <tjddn5242@gmail.com>
2023-01-17 19:39:46 +0900

    sr 제거

--- example/example.py
+++ example/example.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from audiorecorder import audiorecorder
-import speech_recognition as sr
+# import speech_recognition as sr
 
 st.title("Audio Recorder")
 audio = audiorecorder("Click to record", "Recording...")
@@ -13,16 +13,16 @@ if len(audio) > 0:
     wav_file = open("audio.wav", "wb")
     wav_file.write(audio.tobytes())
 
-r = sr.Recognizer()
+# r = sr.Recognizer()
 
 
-with wav_file as source:
-    audio2 = r.record(source)
+# with wav_file as source:
+#     audio2 = r.record(source)
 
 
-result = r.recognize_google(audio2, language = 'ko-KR', show_all=True)
-print(result['alternative'][0]['transcript'])
+# result = r.recognize_google(audio2, language = 'ko-KR', show_all=True)
+# print(result['alternative'][0]['transcript'])
 
-text_output = st.empty()
-text_output.markdown(f"**Text:** ")
+# text_output = st.empty()
+# text_output.markdown(f"**Text:** ")
 
